6770efbcb631b214eb62ab47356dd44aa2c60547
slank-arch <slank.dev@gmail.com>
2016-04-02 00:30:20 +0900

    [FIN] chapter2

--- project/02_day/helloos4/asm.bat
+++ project/02_day/helloos4/asm.bat
@@ -1,1 +1,1 @@
-..\z_tools\nask.exe ipl.nas ipl.bin ipl.lst
+../z_tools/nask.exe ipl.nas ipl.bin ipl.lst
--- project/02_day/helloos4/run.bat
+++ project/02_day/helloos4/run.bat
@@ -1,2 +1,2 @@
-copy helloos.img ..\z_tools\qemu\fdimage0.bin
-..\z_tools\make.exe	-C ../z_tools/qemu
+copy helloos.img ../z_tools/qemu/fdimage0.bin
+../z_tools/make.exe	-C ../z_tools/qemu
